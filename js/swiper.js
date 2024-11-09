174c8ee2614b9b8ba6e900b0065eb2a31f6fd826
myrakuku <149129126+myrakuku@users.noreply.github.com>
2024-11-09 17:51:10 +0800

    first

---
+++ js/swiper.js
@@ -0,0 +1,15 @@
+new Swiper('.hero-banner', {
+    loop: true,
+  
+    // Pagination bullets
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
