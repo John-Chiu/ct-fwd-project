6c976372ee62670bb1a1bfc5be9cb44b8df779f4
John-Chiu <29536154+John-Chiu@users.noreply.github.com>
2024-11-13 01:18:02 +0800

    Add shopping cart function to single product page

---
+++ js/singleProduct.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+  const BTN_ADD_TO_CART_CLASSNAME = 'addToCartBtn';
+
+  document.querySelectorAll(`.${BTN_ADD_TO_CART_CLASSNAME}`)
+    .forEach(btn => {
+      btn.addEventListener('click', handleClickAddToCartBtn);
+    });
+});
+
+
+function handleClickAddToCartBtn(e) {
+  const btn = e.target;
+  const productId = btn.dataset.id;
+  const quantity = Number(document.querySelector('.Cake_number').innerText);
+  addToCart(productId, quantity);
+  btn.disabled = true;
+}
+
+function addToCart(productId, quantity) {
+  if (quantity == undefined)
+    quantity = 1;
+  const cart = getCartFromLocalStorage();
+  const cartItem = getCartItem(productId, cart);
+
+  if (cartItem) {
+    cartItem.quantity += quantity;
+  } else {
+    cart.push({ id: productId, quantity: quantity });
+  }
+  updateLocalStorage(cart);
+}
+
+
+
+function updateLocalStorage(cart) {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+function getCartFromLocalStorage() {
+  let cart = [];
+  if (localStorage.getItem('cart')) {
+    cart = JSON.parse(localStorage.getItem('cart'));
+  }
+  return cart;
+}
+
+function getCartItem(productId, cart) {
+  return cart.find((item) => item.id == productId);
+}
+
