50f9b2526b0439aba212840fab4c3b4881a1812f
Ellieng903 <ellieng903@gmail.com>
2024-11-16 15:34:50 +0800

    mega menu large screen bug

--- js/app.js
+++ js/app.js
@@ -5,6 +5,16 @@ navBtn.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
 
+// Close the mobile menu if the screen width is 768px or wider
+const handleResize = () => {
+    if (window.innerWidth >= 768) {
+        navLinks.classList.remove("active"); // Close the menu
+    }
+};
+
+// Listen for resize events
+window.addEventListener("resize", handleResize);
+
 // add date
 const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
