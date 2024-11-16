aec46a8735adea5d16957bc3fe9abb8981383632
John-Chiu <29536154+John-Chiu@users.noreply.github.com>
2024-11-16 17:23:03 +0800

    Format code

--- js/shopping_cart.js
+++ js/shopping_cart.js
@@ -13,7 +13,6 @@ const BTN_MINUS_CLASSNAME = 'btn-minus';
 const BTN_REMOVE_CLASSNAME = 'btn-remove';
 const BTN_ADD_TO_CART_CLASSNAME = 'addToCartBtn';
 
-
 // DEBUG
 console.log('load cart from localStorage:', getCartFromLocalStorage());
 // get container where cart items render
@@ -24,19 +23,19 @@ renderCartHTML();
 // clear and render shopping cart content:
 // item, price, quantity, subtotal, total
 function renderCartHTML() {
-    const cart = getCartFromLocalStorage();
-    console.log('runtime cart', cart);
-    cartItemListHTML.innerHTML = '';
-    let totalPrice = 0;
-  
-    cart.forEach((item) => {
-      const product = getProduct(item.id);
-      totalPrice += product.price * item.quantity;
-  
-      const newItem = document.createElement('div');
-      newItem.classList.add('purchased-items-box');
-  
-      newItem.innerHTML = `
+  const cart = getCartFromLocalStorage();
+  console.log('runtime cart', cart);
+  cartItemListHTML.innerHTML = '';
+  let totalPrice = 0;
+
+  cart.forEach((item) => {
+    const product = getProduct(item.id);
+    totalPrice += product.price * item.quantity;
+
+    const newItem = document.createElement('div');
+    newItem.classList.add('purchased-items-box');
+
+    newItem.innerHTML = `
       <div>
         <img src="${product.imagePath}" class="item-img" alt="item-img">
       </div>  
@@ -60,101 +59,101 @@ function renderCartHTML() {
         <p>$<span>${product.price * item.quantity}</span></p>
       </div>
       <div class="btn-item-remove">
-        <label class="${BTN_REMOVE_CLASSNAME}" data-id=${product.id}><i class="ph ph-trash"></i></label>
+        <label class="${BTN_REMOVE_CLASSNAME}" data-id=${
+      product.id
+    }><i class="ph ph-trash"></i></label>
       </div>
       `;
-      cartItemListHTML.append(newItem);
-    });
+    cartItemListHTML.append(newItem);
+  });
 
-    document.querySelector('.total-price > span').innerText = totalPrice;
+  document.querySelector('.total-price > span').innerText = totalPrice;
 
-    // + button onclick handler for updating cart item quantity
-    const handleClickPlusQuantityBtn = (e) => {
-      const btn = e.target.parentElement;
-      const id = btn.dataset.id;
-      updateQuantity(id, ADD);
-    };
+  // + button onclick handler for updating cart item quantity
+  const handleClickPlusQuantityBtn = (e) => {
+    const btn = e.target.parentElement;
+    const id = btn.dataset.id;
+    updateQuantity(id, ADD);
+  };
 
-    // - button onclick handler for updating cart item quantity
-    const handleClickMinusQuantityBtn = (e) => {
-      const btn = e.target.parentElement;
-      const id = btn.dataset.id;
-      updateQuantity(id, MINUS);
-    };
-  
-    // add onclick listener for + button to increase cart item quantity
-    const btnsPlus = document.querySelectorAll(`.${BTN_PLUS_CLASSNAME} > i`);
-    btnsPlus.forEach((btn) => {
-      btn.addEventListener('click', handleClickPlusQuantityBtn);
-    });
-  
-    // add onclick listener for - button to decrease cart item quantity
-    const btnsMinus = document.querySelectorAll(`.${BTN_MINUS_CLASSNAME} > i`);
-    btnsMinus.forEach((btn) => {
-      btn.addEventListener('click', handleClickMinusQuantityBtn);
-    });
-  
-  
-    // handler for remove button
-    const handleClickRemoveButton = (e) => {
-      const btn = e.target.parentElement;
-      const id = btn.dataset.id;
-      updateQuantity(id, REMOVE);
-    }
-  
-    // add listener to all remove btn
-    const btnsRemove = document.querySelectorAll(`.${BTN_REMOVE_CLASSNAME} > i`);
-    btnsRemove.forEach((btn) => {
-      btn.addEventListener('click', handleClickRemoveButton);
-    });
+  // - button onclick handler for updating cart item quantity
+  const handleClickMinusQuantityBtn = (e) => {
+    const btn = e.target.parentElement;
+    const id = btn.dataset.id;
+    updateQuantity(id, MINUS);
+  };
+
+  // add onclick listener for + button to increase cart item quantity
+  const btnsPlus = document.querySelectorAll(`.${BTN_PLUS_CLASSNAME} > i`);
+  btnsPlus.forEach((btn) => {
+    btn.addEventListener('click', handleClickPlusQuantityBtn);
+  });
+
+  // add onclick listener for - button to decrease cart item quantity
+  const btnsMinus = document.querySelectorAll(`.${BTN_MINUS_CLASSNAME} > i`);
+  btnsMinus.forEach((btn) => {
+    btn.addEventListener('click', handleClickMinusQuantityBtn);
+  });
+
+  // handler for remove button
+  const handleClickRemoveButton = (e) => {
+    const btn = e.target.parentElement;
+    const id = btn.dataset.id;
+    updateQuantity(id, REMOVE);
+  };
+
+  // add listener to all remove btn
+  const btnsRemove = document.querySelectorAll(`.${BTN_REMOVE_CLASSNAME} > i`);
+  btnsRemove.forEach((btn) => {
+    btn.addEventListener('click', handleClickRemoveButton);
+  });
+}
+
+// update quantity of item in cart
+function updateQuantity(productId, action) {
+  let cart = getCartFromLocalStorage();
+  const cartItem = getCartItem(productId, cart);
+
+  switch (action) {
+    case ADD:
+      cartItem.quantity++;
+      break;
+    case MINUS:
+      cartItem.quantity--;
+      if (cartItem.quantity <= 0) {
+        cart = cart.filter((x) => x !== cartItem);
+      }
+      break;
+    case REMOVE:
+      cart = cart.filter((x) => x !== cartItem);
+      break;
+    default:
+      break;
   }
-  
-  // update quantity of item in cart
-  function updateQuantity(productId, action) {
-    let cart = getCartFromLocalStorage();
-    const cartItem = getCartItem(productId, cart);
-    
-    switch (action) {
-      case ADD:
-        cartItem.quantity++;
-        break;
-      case MINUS:
-        cartItem.quantity--;
-        if (cartItem.quantity <= 0) {
-          cart = cart.filter(x => x !== cartItem);
-        }
-        break;
-      case REMOVE:
-        cart = cart.filter(x => x !== cartItem);
-        break;
-      default:
-        break;
-    }
-    updateLocalStorage(cart);
-    renderCartHTML();
+  updateLocalStorage(cart);
+  renderCartHTML();
+}
+
+// =======  helper function =======
+function updateLocalStorage(cart) {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+function getCartFromLocalStorage() {
+  let cart = [];
+  if (localStorage.getItem('cart')) {
+    cart = JSON.parse(localStorage.getItem('cart'));
   }
-  
-  // =======  helper function =======
-  function updateLocalStorage(cart) {
-    localStorage.setItem('cart', JSON.stringify(cart));
-  }
-  
-  function getCartFromLocalStorage() {
-    let cart = [];
-    if (localStorage.getItem('cart')) {
-      cart = JSON.parse(localStorage.getItem('cart'));
-    }
-    return cart;
-  }
-  
-  function getProduct(productId) {
-    return products.find((p) => {
-      return p.id == productId;
-    });
-  }
-  
-  function getCartItem(productId, cart) {
-    return cart.find((item) => item.id == productId);
-  }
-  // =======  helper function END =======
-  
+  return cart;
+}
+
+function getProduct(productId) {
+  return products.find((p) => {
+    return p.id == productId;
+  });
+}
+
+function getCartItem(productId, cart) {
+  return cart.find((item) => item.id == productId);
+}
+// =======  helper function END =======
